cea444d8ead3bfd8c85f657edbc2638b74d96e15
paolor72 <paolor72@gmail.com>
2025-03-30 17:47:38 +0200

    aggiornamenti page bug  package

--- app/blobs/page.jsx
+++ app/blobs/page.jsx
@@ -1,39 +1,33 @@
 import { Markdown } from 'components/markdown';
 import { ShapeEditor } from './editor';
 import { ContextAlert } from 'components/context-alert';
-import { getNetlifyContext, uploadDisabled } from 'utils';
 
 export const metadata = {
     title: 'Blobs'
 };
 
 const explainer = `
-[Netlify Blobs](https://docs.netlify.com/blobs/overview/) provides an object store for any kind of data, be it JSON, binary, 
-or [really](https://mk.gg/projects/chalkstream) anything else ([really!](https://mk.gg/projects/turbofan)). In this example, the blob store is used to **hold the data of user-generated random blobby shapes**.
+Questo esempio mostra come utilizzare un oggetto store per qualsiasi tipo di dato, sia esso JSON, binario, 
+o qualsiasi altra cosa. In questo esempio, lo store è utilizzato per **tenere i dati di forme blobby generate dagli utenti**.
 
-Using the blob store is basically zero-config. Below is a Next.js Server Action to upload data (see \`app/blobs/actions.js\`). 
-When deployed to Netlify, the Server Action is run by serverless functions, and all context required for the blob service is set-up automatically.
+L'uso dello store è praticamente zero-config. Di seguito è riportato un esempio di come caricare dati.
 
 ~~~js
 'use server';
-import { getStore } from '@netlify/blobs';
-
-// TODO: Always be sanitizing data in real sites!
+// Funzione per caricare forme
 export async function uploadShape({ shapeData }) {
-    const blobStore = getStore('shapes');
-    const key = data.name;
-    await blobStore.setJSON(key, shapeData);
+    // Logica per caricare i dati
 }
 ~~~
 
-Click "Randomize" to get a shape you like, then hit "Upload".
-Choose any existing object to view it.
+Clicca "Randomize" per ottenere una forma che ti piace, poi premi "Upload".
+Scegli qualsiasi oggetto esistente per visualizzarlo.
 `;
 
 const uploadDisabledText = `
-User uploads are disabled in this site. To run your own and try it out: 
-<a href="https://app.netlify.com/start/deploy?repository=https://github.com/netlify-templates/next-platform-starter">
-<img src="https://www.netlify.com/img/deploy/button.svg" style="display: inline;" alt="Deploy to Netlify" />
+Gli upload degli utenti sono disabilitati in questo sito. Per eseguire il tuo e provare: 
+<a href="https://app.example.com/start/deploy?repository=https://github.com/example/your-repo">
+<img src="https://www.example.com/img/deploy/button.svg" style="display: inline;" alt="Deploy" />
 </a>
 `;
 
